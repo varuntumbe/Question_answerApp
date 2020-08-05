0cf39679dd0c687dce7fe0d40973d74c218137a2
Varun <varuntumbe1@gmail.com>
2020-08-06 01:52:58 +0530

    20% has completed and templates have been refactored

--- app.py
+++ app.py
@@ -1,27 +1,65 @@
-from flask import Flask,render_template,g
+from flask import Flask,render_template,g,request,session,redirect,url_for
 import database
+from werkzeug.security import generate_password_hash,check_password_hash
+import os
 
 app=Flask(__name__)
 app.config['DEBUG']=True
+app.config['SECRET_KEY']=os.urandom(24)
 
 #Below function tears down database connection 
 # everytime our request-response context ends
 @app.teardown_appcontext
-def close_db():
+def close_db(err):
     if hasattr(g,'sqlite_db'):
         g.sqlite_db.close()
 
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    user=None
+    if 'user' in session:
+        user=session['user']
+    return render_template('home.html',user=user)
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def regi():
+    if request.method=='POST':
+        hashed_password=generate_password_hash(request.form.get('password'),method='sha256')
+        name=request.form.get('name')
+
+        #Database part
+        conn=database.get_db()
+        cur=conn.cursor()
+        cur.execute(""" 
+            INSERT INTO users (name,password,expert,admin)
+            VALUES (?,?,?,?)
+        """,[name,hashed_password,False,False])
+        conn.commit()
+        #connection will be automatically tear down when return statement executes
+        return render_template('home.html')
+
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
+    if request.method=='POST':
+        name=request.form.get('name')
+        password=request.form.get('password')
+        #database part
+        conn=database.get_db()
+        cur=conn.cursor()
+        cur.execute("""
+            SELECT id,name,password FROM users 
+            WHERE name=(?)
+        """,(name,))
+        user_info=cur.fetchone()
+        if check_password_hash(user_info['password'],password):
+            session['user']=user_info['name']
+            return '<h1>matched!!</h1>'
+        else:
+            return '<h1>didnt match</h1>'
+
     return render_template('login.html')
 
 @app.route('/question')
@@ -43,6 +81,12 @@ def unanswered():
 @app.route('/users')
 def users():
     return render_template('users.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('index'),code=302)
+
 
 if __name__ == "__main__":
     app.run()
--- database.py
+++ database.py
@@ -6,17 +6,30 @@ from flask import g
 def connect_db():
     conn=sqlite3.connect('data.db')
     conn.row_factory=sqlite3.Row
+    create_table(conn)
     return conn
 
 def get_db():
     if not hasattr(g,'sqlite_db'):
-        g['sqlite_db']=connect_db()
-    return g['sqlite_db']
+        g.sqlite_db=connect_db()
+    return g.sqlite_db
 
 #Database function to create tables in database
 def create_table(conn):
     cur=conn.cursor()
     cur.executescript(""" 
-
+        CREATE TABLE IF NOT EXISTS users(
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+          name TEXT NOT NULL,
+          password TEXT NOT NULL,
+          expert BOOLEAN NOT NULL,
+          admin BOOLEAN NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS questions(
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+          question_text TEXT NOT NULL,
+          answer_text TEXT,
+          user_id INTEGER NOT NULL,
+          expert_id INTEGER NOT NULL
+        )
     """)
-    conn.close()
