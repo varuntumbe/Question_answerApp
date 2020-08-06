225d7fbaa50f545bafed581e5a5b637211a796a4
Varun <varuntumbe1@gmail.com>
2020-08-06 12:41:54 +0530

    30% finished

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def close_db(err):
 
 @app.route('/')
 def index():
-    user=None
-    if 'user' in session:
-        user=session['user']
-    return render_template('home.html',user=user)
+    user_info=get_current_user()
+    if user_info:
+        return render_template('home.html',user_info=user_info)
+    return render_template('home.html',user_info=user_info)
 
 @app.route('/register',methods=['GET','POST'])
 def regi():
+#    user_info=get_current_user()  
     if request.method=='POST':
         hashed_password=generate_password_hash(request.form.get('password'),method='sha256')
         name=request.form.get('name')
@@ -37,12 +38,13 @@ def regi():
         """,[name,hashed_password,False,False])
         conn.commit()
         #connection will be automatically tear down when return statement executes
-        return render_template('home.html')
+        return render_template('home.html',user_info=None)
 
-    return render_template('register.html')
+    return render_template('register.html',user_info=None)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    user_info=get_current_user()
     if request.method=='POST':
         name=request.form.get('name')
         password=request.form.get('password')
@@ -54,38 +56,99 @@ def login():
             WHERE name=(?)
         """,(name,))
         user_info=cur.fetchone()
-        if check_password_hash(user_info['password'],password):
+        if user_info and check_password_hash(user_info['password'],password):
             session['user']=user_info['name']
-            return '<h1>matched!!</h1>'
+            return redirect(url_for('index'),code=302)
         else:
-            return '<h1>didnt match</h1>'
+            return render_template('login.html',user_info=user_info)
 
-    return render_template('login.html')
+    return render_template('login.html',user_info=user_info)
 
 @app.route('/question')
 def question():
-    return render_template('question.html')
+    user_info=get_current_user()
+    return render_template('question.html',user_info=user_info)
 
 @app.route('/answer')
 def answer():
-    return render_template('answer.html')
+    user_info=get_current_user()
+    return render_template('answer.html',user_info=user_info)
 
 @app.route('/ask')
 def ask():
-    return render_template('ask.html')
+    user_info=get_current_user()
+    if user_info:
+        return render_template('ask.html',user_info=user_info)
+    else:
+        return redirect(url_for('login'),code=302)
 
 @app.route('/unanswered')
 def unanswered():
-    return render_template('unanswered.html')
+    user_info=get_current_user()
+    return render_template('unanswered.html',user_info=user_info)
 
-@app.route('/users')
+@app.route('/users',methods=['GET','POST'])
 def users():
-    return render_template('users.html')
+    user_info=get_current_user()
+
+    #database part
+    conn=database.get_db()
+    cur=conn.cursor()
+    cur.execute(""" 
+        SELECT id,name,expert,admin from users 
+    """)
+    users=cur.fetchall()
+    return render_template('users.html',user_info=user_info,users=users)
 
 @app.route('/logout')
 def logout():
+#    user_info=get_current_user()
     session.pop('user',None)
     return redirect(url_for('index'),code=302)
+
+
+@app.route('/promote/<username>')
+def promote(username):
+    user_info=get_current_user()
+    act=request.args.get('action')
+    if user_info and username:
+
+        if act=='add':
+            #database part
+            conn=database.get_db()
+            cur=conn.cursor()
+            cur.execute(""" 
+                UPDATE users SET expert=(?) 
+                WHERE name=(?)
+            """,(True,username))
+            conn.commit()
+            return redirect(url_for('users'),code=302)
+        else:
+            #database part
+            conn=database.get_db()
+            cur=conn.cursor()
+            cur.execute(""" 
+                UPDATE users SET expert=(?) 
+                WHERE name=(?)
+            """,(False,username))
+            conn.commit()
+            return redirect(url_for('users'),code=302)            
+
+
+
+#some useful functions
+def get_current_user():
+    user_info=None
+    if 'user' in session:
+
+        #database part
+        conn=database.get_db()
+        cur=conn.cursor()
+        cur.execute(""" 
+            SELECT * FROM users WHERE NAME=(?)
+        """,(session['user'],))
+        user_info=cur.fetchone()
+    return user_info
 
 
 if __name__ == "__main__":
